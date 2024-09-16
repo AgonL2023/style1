a4a94fa347ea7ac57502fd46def877e8a0ddb24a
AgonL2023 <139693542+AgonL2023@users.noreply.github.com>
2024-09-16 03:19:17 +0200

    Add files via upload

--- utils.js
+++ utils.js
@@ -1,303 +1,287 @@
-/*!
- * express
- * Copyright(c) 2009-2013 TJ Holowaychuk
- * Copyright(c) 2014-2015 Douglas Christopher Wilson
- * MIT Licensed
- */
-
-'use strict';
-
+import * as qs from 'qs';
+const OPTIONS_KEYS = [
+    'apiKey',
+    'idempotencyKey',
+    'stripeAccount',
+    'apiVersion',
+    'maxNetworkRetries',
+    'timeout',
+    'host',
+];
+export function isOptionsHash(o) {
+    return (o &&
+        typeof o === 'object' &&
+        OPTIONS_KEYS.some((prop) => Object.prototype.hasOwnProperty.call(o, prop)));
+}
 /**
- * Module dependencies.
- * @api private
+ * Stringifies an Object, accommodating nested objects
+ * (forming the conventional key 'parent[child]=value')
  */
-
-var Buffer = require('safe-buffer').Buffer
-var contentDisposition = require('content-disposition');
-var contentType = require('content-type');
-var deprecate = require('depd')('express');
-var flatten = require('array-flatten');
-var mime = require('send').mime;
-var etag = require('etag');
-var proxyaddr = require('proxy-addr');
-var qs = require('qs');
-var querystring = require('querystring');
-
+export function stringifyRequestData(data) {
+    return (qs
+        .stringify(data, {
+        serializeDate: (d) => Math.floor(d.getTime() / 1000).toString(),
+    })
+        // Don't use strict form encoding by changing the square bracket control
+        // characters back to their literals. This is fine by the server, and
+        // makes these parameter strings easier to read.
+        .replace(/%5B/g, '[')
+        .replace(/%5D/g, ']'));
+}
 /**
- * Return strong ETag for `body`.
- *
- * @param {String|Buffer} body
- * @param {String} [encoding]
- * @return {String}
- * @api private
+ * Outputs a new function with interpolated object property values.
+ * Use like so:
+ *   const fn = makeURLInterpolator('some/url/{param1}/{param2}');
+ *   fn({ param1: 123, param2: 456 }); // => 'some/url/123/456'
  */
-
-exports.etag = createETagGenerator({ weak: false })
-
-/**
- * Return weak ETag for `body`.
- *
- * @param {String|Buffer} body
- * @param {String} [encoding]
- * @return {String}
- * @api private
- */
-
-exports.wetag = createETagGenerator({ weak: true })
-
-/**
- * Check if `path` looks absolute.
- *
- * @param {String} path
- * @return {Boolean}
- * @api private
- */
-
-exports.isAbsolute = function(path){
-  if ('/' === path[0]) return true;
-  if (':' === path[1] && ('\\' === path[2] || '/' === path[2])) return true; // Windows device path
-  if ('\\\\' === path.substring(0, 2)) return true; // Microsoft Azure absolute path
-};
-
-/**
- * Flatten the given `arr`.
- *
- * @param {Array} arr
- * @return {Array}
- * @api private
- */
-
-exports.flatten = deprecate.function(flatten,
-  'utils.flatten: use array-flatten npm module instead');
-
-/**
- * Normalize the given `type`, for example "html" becomes "text/html".
- *
- * @param {String} type
- * @return {Object}
- * @api private
- */
-
-exports.normalizeType = function(type){
-  return ~type.indexOf('/')
-    ? acceptParams(type)
-    : { value: mime.lookup(type), params: {} };
-};
-
-/**
- * Normalize `types`, for example "html" becomes "text/html".
- *
- * @param {Array} types
- * @return {Array}
- * @api private
- */
-
-exports.normalizeTypes = function(types){
-  var ret = [];
-
-  for (var i = 0; i < types.length; ++i) {
-    ret.push(exports.normalizeType(types[i]));
-  }
-
-  return ret;
-};
-
-/**
- * Generate Content-Disposition header appropriate for the filename.
- * non-ascii filenames are urlencoded and a filename* parameter is added
- *
- * @param {String} filename
- * @return {String}
- * @api private
- */
-
-exports.contentDisposition = deprecate.function(contentDisposition,
-  'utils.contentDisposition: use content-disposition npm module instead');
-
-/**
- * Parse accept params `str` returning an
- * object with `.value`, `.quality` and `.params`.
- *
- * @param {String} str
- * @return {Object}
- * @api private
- */
-
-function acceptParams (str) {
-  var parts = str.split(/ *; */);
-  var ret = { value: parts[0], quality: 1, params: {} }
-
-  for (var i = 1; i < parts.length; ++i) {
-    var pms = parts[i].split(/ *= */);
-    if ('q' === pms[0]) {
-      ret.quality = parseFloat(pms[1]);
-    } else {
-      ret.params[pms[0]] = pms[1];
+export const makeURLInterpolator = (() => {
+    const rc = {
+        '\n': '\\n',
+        '"': '\\"',
+        '\u2028': '\\u2028',
+        '\u2029': '\\u2029',
+    };
+    return (str) => {
+        const cleanString = str.replace(/["\n\r\u2028\u2029]/g, ($0) => rc[$0]);
+        return (outputs) => {
+            return cleanString.replace(/\{([\s\S]+?)\}/g, ($0, $1) => 
+            // @ts-ignore
+            encodeURIComponent(outputs[$1] || ''));
+        };
+    };
+})();
+export function extractUrlParams(path) {
+    const params = path.match(/\{\w+\}/g);
+    if (!params) {
+        return [];
     }
-  }
-
-  return ret;
+    return params.map((param) => param.replace(/[{}]/g, ''));
 }
-
 /**
- * Compile "etag" value to function.
+ * Return the data argument from a list of arguments
  *
- * @param  {Boolean|String|Function} val
- * @return {Function}
- * @api private
+ * @param {object[]} args
+ * @returns {object}
  */
-
-exports.compileETag = function(val) {
-  var fn;
-
-  if (typeof val === 'function') {
-    return val;
-  }
-
-  switch (val) {
-    case true:
-    case 'weak':
-      fn = exports.wetag;
-      break;
-    case false:
-      break;
-    case 'strong':
-      fn = exports.etag;
-      break;
-    default:
-      throw new TypeError('unknown value for etag function: ' + val);
-  }
-
-  return fn;
+export function getDataFromArgs(args) {
+    if (!Array.isArray(args) || !args[0] || typeof args[0] !== 'object') {
+        return {};
+    }
+    if (!isOptionsHash(args[0])) {
+        return args.shift();
+    }
+    const argKeys = Object.keys(args[0]);
+    const optionKeysInArgs = argKeys.filter((key) => OPTIONS_KEYS.includes(key));
+    // In some cases options may be the provided as the first argument.
+    // Here we're detecting a case where there are two distinct arguments
+    // (the first being args and the second options) and with known
+    // option keys in the first so that we can warn the user about it.
+    if (optionKeysInArgs.length > 0 &&
+        optionKeysInArgs.length !== argKeys.length) {
+        emitWarning(`Options found in arguments (${optionKeysInArgs.join(', ')}). Did you mean to pass an options object? See https://github.com/stripe/stripe-node/wiki/Passing-Options.`);
+    }
+    return {};
 }
-
 /**
- * Compile "query parser" value to function.
- *
- * @param  {String|Function} val
- * @return {Function}
- * @api private
+ * Return the options hash from a list of arguments
  */
-
-exports.compileQueryParser = function compileQueryParser(val) {
-  var fn;
-
-  if (typeof val === 'function') {
-    return val;
-  }
-
-  switch (val) {
-    case true:
-    case 'simple':
-      fn = querystring.parse;
-      break;
-    case false:
-      fn = newObject;
-      break;
-    case 'extended':
-      fn = parseExtendedQueryString;
-      break;
-    default:
-      throw new TypeError('unknown value for query parser function: ' + val);
-  }
-
-  return fn;
+export function getOptionsFromArgs(args) {
+    const opts = {
+        auth: null,
+        host: null,
+        headers: {},
+        settings: {},
+    };
+    if (args.length > 0) {
+        const arg = args[args.length - 1];
+        if (typeof arg === 'string') {
+            opts.auth = args.pop();
+        }
+        else if (isOptionsHash(arg)) {
+            const params = Object.assign({}, args.pop());
+            const extraKeys = Object.keys(params).filter((key) => !OPTIONS_KEYS.includes(key));
+            if (extraKeys.length) {
+                emitWarning(`Invalid options found (${extraKeys.join(', ')}); ignoring.`);
+            }
+            if (params.apiKey) {
+                opts.auth = params.apiKey;
+            }
+            if (params.idempotencyKey) {
+                opts.headers['Idempotency-Key'] = params.idempotencyKey;
+            }
+            if (params.stripeAccount) {
+                opts.headers['Stripe-Account'] = params.stripeAccount;
+            }
+            if (params.apiVersion) {
+                opts.headers['Stripe-Version'] = params.apiVersion;
+            }
+            if (Number.isInteger(params.maxNetworkRetries)) {
+                opts.settings.maxNetworkRetries = params.maxNetworkRetries;
+            }
+            if (Number.isInteger(params.timeout)) {
+                opts.settings.timeout = params.timeout;
+            }
+            if (params.host) {
+                opts.host = params.host;
+            }
+        }
+    }
+    return opts;
 }
-
 /**
- * Compile "proxy trust" value to function.
- *
- * @param  {Boolean|String|Number|Array|Function} val
- * @return {Function}
- * @api private
+ * Provide simple "Class" extension mechanism.
+ * <!-- Public API accessible via Stripe.StripeResource.extend -->
  */
-
-exports.compileTrust = function(val) {
-  if (typeof val === 'function') return val;
-
-  if (val === true) {
-    // Support plain true/false
-    return function(){ return true };
-  }
-
-  if (typeof val === 'number') {
-    // Support trusting hop count
-    return function(a, i){ return i < val };
-  }
-
-  if (typeof val === 'string') {
-    // Support comma-separated values
-    val = val.split(',')
-      .map(function (v) { return v.trim() })
-  }
-
-  return proxyaddr.compile(val || []);
+export function protoExtend(sub) {
+    // eslint-disable-next-line @typescript-eslint/no-this-alias
+    const Super = this;
+    const Constructor = Object.prototype.hasOwnProperty.call(sub, 'constructor')
+        ? sub.constructor
+        : function (...args) {
+            Super.apply(this, args);
+        };
+    // This initialization logic is somewhat sensitive to be compatible with
+    // divergent JS implementations like the one found in Qt. See here for more
+    // context:
+    //
+    // https://github.com/stripe/stripe-node/pull/334
+    Object.assign(Constructor, Super);
+    Constructor.prototype = Object.create(Super.prototype);
+    Object.assign(Constructor.prototype, sub);
+    return Constructor;
 }
-
 /**
- * Set the charset in a given Content-Type string.
- *
- * @param {String} type
- * @param {String} charset
- * @return {String}
- * @api private
+ * Remove empty values from an object
  */
-
-exports.setCharset = function setCharset(type, charset) {
-  if (!type || !charset) {
-    return type;
-  }
-
-  // parse type
-  var parsed = contentType.parse(type);
-
-  // set charset
-  parsed.parameters.charset = charset;
-
-  // format type
-  return contentType.format(parsed);
-};
-
-/**
- * Create an ETag generator function, generating ETags with
- * the given options.
- *
- * @param {object} options
- * @return {function}
- * @private
- */
-
-function createETagGenerator (options) {
-  return function generateETag (body, encoding) {
-    var buf = !Buffer.isBuffer(body)
-      ? Buffer.from(body, encoding)
-      : body
-
-    return etag(buf, options)
-  }
+export function removeNullish(obj) {
+    if (typeof obj !== 'object') {
+        throw new Error('Argument must be an object');
+    }
+    return Object.keys(obj).reduce((result, key) => {
+        if (obj[key] != null) {
+            result[key] = obj[key];
+        }
+        return result;
+    }, {});
 }
-
 /**
- * Parse an extended query string with qs.
- *
- * @param {String} str
- * @return {Object}
- * @private
+ * Normalize standard HTTP Headers:
+ * {'foo-bar': 'hi'}
+ * becomes
+ * {'Foo-Bar': 'hi'}
  */
-
-function parseExtendedQueryString(str) {
-  return qs.parse(str, {
-    allowPrototypes: true
-  });
+export function normalizeHeaders(obj) {
+    if (!(obj && typeof obj === 'object')) {
+        return obj;
+    }
+    return Object.keys(obj).reduce((result, header) => {
+        result[normalizeHeader(header)] = obj[header];
+        return result;
+    }, {});
 }
-
 /**
- * Return new empty object.
- *
- * @return {Object}
- * @api private
+ * Stolen from https://github.com/marten-de-vries/header-case-normalizer/blob/master/index.js#L36-L41
+ * without the exceptions which are irrelevant to us.
  */
-
-function newObject() {
-  return {};
+export function normalizeHeader(header) {
+    return header
+        .split('-')
+        .map((text) => text.charAt(0).toUpperCase() + text.substr(1).toLowerCase())
+        .join('-');
+}
+export function callbackifyPromiseWithTimeout(promise, callback) {
+    if (callback) {
+        // Ensure callback is called outside of promise stack.
+        return promise.then((res) => {
+            setTimeout(() => {
+                callback(null, res);
+            }, 0);
+        }, (err) => {
+            setTimeout(() => {
+                callback(err, null);
+            }, 0);
+        });
+    }
+    return promise;
+}
+/**
+ * Allow for special capitalization cases (such as OAuth)
+ */
+export function pascalToCamelCase(name) {
+    if (name === 'OAuth') {
+        return 'oauth';
+    }
+    else {
+        return name[0].toLowerCase() + name.substring(1);
+    }
+}
+export function emitWarning(warning) {
+    if (typeof process.emitWarning !== 'function') {
+        return console.warn(`Stripe: ${warning}`); /* eslint-disable-line no-console */
+    }
+    return process.emitWarning(warning, 'Stripe');
+}
+export function isObject(obj) {
+    const type = typeof obj;
+    return (type === 'function' || type === 'object') && !!obj;
+}
+// For use in multipart requests
+export function flattenAndStringify(data) {
+    const result = {};
+    const step = (obj, prevKey) => {
+        Object.keys(obj).forEach((key) => {
+            // @ts-ignore
+            const value = obj[key];
+            const newKey = prevKey ? `${prevKey}[${key}]` : key;
+            if (isObject(value)) {
+                if (!(value instanceof Uint8Array) &&
+                    !Object.prototype.hasOwnProperty.call(value, 'data')) {
+                    // Non-buffer non-file Objects are recursively flattened
+                    return step(value, newKey);
+                }
+                else {
+                    // Buffers and file objects are stored without modification
+                    result[newKey] = value;
+                }
+            }
+            else {
+                // Primitives are converted to strings
+                result[newKey] = String(value);
+            }
+        });
+    };
+    step(data, null);
+    return result;
+}
+export function validateInteger(name, n, defaultVal) {
+    if (!Number.isInteger(n)) {
+        if (defaultVal !== undefined) {
+            return defaultVal;
+        }
+        else {
+            throw new Error(`${name} must be an integer`);
+        }
+    }
+    return n;
+}
+export function determineProcessUserAgentProperties() {
+    return typeof process === 'undefined'
+        ? {}
+        : {
+            lang_version: process.version,
+            platform: process.platform,
+        };
+}
+/**
+ * Joins an array of Uint8Arrays into a single Uint8Array
+ */
+export function concat(arrays) {
+    const totalLength = arrays.reduce((len, array) => len + array.length, 0);
+    const merged = new Uint8Array(totalLength);
+    let offset = 0;
+    arrays.forEach((array) => {
+        merged.set(array, offset);
+        offset += array.length;
+    });
+    return merged;
 }
